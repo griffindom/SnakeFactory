92f39a370685adc7468a19b68d4eeebeefbd32ef
Dominguez, Griffin Alexander <gdominguez8@gatech.edu>
2021-03-14 23:00:45 -0400

    Fixs version

--- SnakeDungeon/src/tests/MazeTest.java
+++ SnakeDungeon/src/tests/MazeTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
 import sample.Main;
-import sample.RoomController;
 
 import static org.testfx.api.FxAssert.verifyThat;
 
@@ -18,7 +17,7 @@ public class MazeTest extends ApplicationTest {
     }
 
     @Test
-    public void testGoBack() {
+    public void testFirstRoom() {
         clickOn("#startButton");
         clickOn("#farmerName");
         write("Not empty");
@@ -39,4 +38,109 @@ public class MazeTest extends ApplicationTest {
         }
         verifyThat("You escaped the Snake Dungeon!", NodeMatchers.isNotNull());
     }
+
+    @Test
+    public void testDoor1Through() {
+        clickOn("#startButton");
+        clickOn("#farmerName");
+        write("Not empty");
+        clickOn("#longSwordButton");
+        clickOn("#begin");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+    }
+
+    @Test
+    public void testDoor1GoBackFunc() {
+        clickOn("#startButton");
+        clickOn("#farmerName");
+        write("Not empty");
+        clickOn("#longSwordButton");
+        clickOn("#begin");
+        clickOn("#door1");
+        clickOn("#goBack");
+        clickOn("#door1");
+
+    }
+
+    @Test
+    public void testDoor3Through() {
+        clickOn("#startButton");
+        clickOn("#farmerName");
+        write("Not empty");
+        clickOn("#longSwordButton");
+        clickOn("#begin");
+        for (int i = 0; i < 10; i++) {
+            if (false) {
+                clickOn("#door3");
+            } else {
+                clickOn("#door1");
+            }
+        }
+    }
+
+    @Test
+    public void testDoor3GoBackFunc() {
+        clickOn("#startButton");
+        clickOn("#farmerName");
+        write("Not empty");
+        clickOn("#longSwordButton");
+        clickOn("#begin");
+        if (lookup("#door3").query() != null) {
+            clickOn("#door3");
+        } else {
+            clickOn("#door1");
+        }
+        clickOn("#goBack");
+        if (lookup("#door3").query() != null) {
+            clickOn("#door3");
+        } else {
+            clickOn("#door1");
+        }
+    }
+
+    @Test
+    public void testEndScreenMultipleDoors() {
+        clickOn("#startButton");
+        clickOn("#farmerName");
+        write("Gerald");
+        clickOn("#daggerButton");
+        clickOn("#begin");
+        for (int i = 0; i < 10; i++) {
+            if (false) {
+                clickOn("#door2");
+            } else {
+                clickOn("#door1");
+            }
+        }
+        verifyThat("You escaped the Snake Dungeon!", NodeMatchers.isNotNull());
+    }
+
+    @Test
+    public void testRoom1toEndScreen() {
+        clickOn("#startButton");
+        clickOn("#farmerName");
+        write("Aslan");
+        clickOn("#daggerButton");
+        clickOn("#begin");
+        clickOn("#door4");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        clickOn("#door1");
+        verifyThat("You escaped the Snake Dungeon!", NodeMatchers.isNotNull());
+    }
+
 }
