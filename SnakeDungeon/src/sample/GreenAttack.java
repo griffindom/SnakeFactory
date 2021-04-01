90f886833d9137bd9a97c1aa82b5457b6cfd8ada
Dominguez, Griffin Alexander <gdominguez8@gatech.edu>
2021-04-01 11:44:34 -0400

    Delete GreenAttack.java

--- SnakeDungeon/src/sample/GreenAttack.java
+++
@@ -1,8 +0,0 @@
-package sample;
-
-public class GreenAttack implements AttackAbility {
-    @Override
-    public void attack() {
-        System.out.println("Im a weak little green snake");
-    }
-}
